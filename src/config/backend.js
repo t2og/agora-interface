cc7a515f302c50ad0286680b4c5158caacc4127c
njxieyt <njxieyt@hotmail.com>
2023-07-11 16:33:49 +0800

    get data from subgraph

--- src/config/backend.js
+++ src/config/backend.js
@@ -1,4 +1,6 @@
 export const ENDPOINT = 'https://app.agorasea.top/';
 export const GET_LIST = 'api/list';
+export const GET_ITEMS  = 'api/items';
 
 export const GET_LIST_URL = ENDPOINT + GET_LIST;
+export const GET_ITEMS_URL = ENDPOINT + GET_ITEMS;
